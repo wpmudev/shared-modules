bad5bbc3a8898b03a7df55db04c9d807e91324a7
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-02-12 21:30:40 -0500

    🔧 Builder for components

---
+++ packages/module-builder/lib/module-builder.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+const rollup = require( "rollup" );
+const path = require( "path" );
+const resolve = require( "@rollup/plugin-node-resolve" ).default;
+const babel = require( "@rollup/plugin-babel" ).default;
+
+const currentWorkingPath = process.cwd();
+const { main, name } = require( path.join( currentWorkingPath, "package.json" ) );
+
+const inputPath = path.join( currentWorkingPath, main );
+
+// Little workaround to get package name without scope.
+const fileName = name.replace( "@wpmudev/", "" );
+
+// See below for details on the options.
+const inputOptions = {
+	input: inputPath,
+	external: [
+		"react"
+	],
+	plugins: [
+		resolve(),
+		babel({
+			presets: [
+				"@babel/preset-env",
+				"@babel/preset-react"
+			],
+			babelHelpers: "bundled"
+		}),
+	],
+};
+
+const outputOptions = [
+	{
+		file: `dist/${fileName}.cjs.js`,
+		format: "cjs"
+	},
+	{
+		file: `dist/${fileName}.esm.js`,
+		format: "esm"
+	}
+];
+
+async function build() {
+	// Create bundle.
+	const bundle = await rollup.rollup( inputOptions );
+	// Loop through the options and write individual bundles.
+	outputOptions.forEach( async ( options ) => {
+		await bundle.write( options );
+	});
+}
+
+build();
