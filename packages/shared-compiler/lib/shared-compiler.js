4e3cf93acbd70b4914f49d44469817984a146024
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-02-18 18:27:20 -0500

    🔧 Compile packages JS with Rollup.

--- packages/shared-compiler/lib/shared-compiler.js
+++ packages/shared-compiler/lib/shared-compiler.js
@@ -1,2 +1,55 @@
 #!/usr/bin/env node
-console.log( "Woo" );
+const rollup = require( "rollup" );
+const path = require( "path" );
+const resolve = require( "@rollup/plugin-node-resolve" ).default;
+const babel = require( "@rollup/plugin-babel" ).default;
+
+const currentWorkingPath = process.cwd();
+const { main, name } = require( path.join( currentWorkingPath, "package.json" ) );
+
+const inputPath = path.join( currentWorkingPath, main );
+
+// Get package name without scope.
+const fileName = name.replace( "@wpmudev/", "" );
+
+// Input options.
+const inputOptions = {
+	input: inputPath,
+	external: [
+		"react"
+	],
+	plugins: [
+		resolve(),
+		babel({
+			presets: [
+				"@babel/preset-env",
+				"@babel/preset-react"
+			],
+			babelHelpers: "bundled"
+		})
+	]
+};
+
+// Output options.
+const outputOptions = [
+	{
+		file: `dist/${fileName}.cjs.js`,
+		format: "cjs"
+	},
+	{
+		file: `dist/${fileName}.esm.js`,
+		format: "esm"
+	}
+];
+
+async function build() {
+	// Create bundle.
+	const bundle = await rollup.rollup( inputOptions );
+
+	// Loop through the options and write individual bundles.
+	outputOptions.forEach( async ( options ) => {
+		await bundle.write( options );
+	});
+}
+
+build();
