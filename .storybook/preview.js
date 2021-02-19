b73504b8bc2cc2c33a2eb55830510bb4dbb80160
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-02-19 09:42:28 -0500

    🔧 Enable SUI styles on Storybook preview mode.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,24 @@
+import React from "react";
+import "@wpmudev/shared-ui/dist/css/shared-ui.min.css"; // Get SUI styles.
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+const Wrapper = ({ children }) => {
+	return (
+		<div className="sui-2-10-4">
+			<div className="sui-wrap">
+				{ children }
+			</div>
+		</div>
+	);
+};
+
+export const decorators = [
+	( Story ) => (
+		<Wrapper>
+			<Story />
+		</Wrapper>
+	),
+];
