b9487981a290ee322efbe5dcaf2a2d1d7cd0a70c
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-02-18 20:54:52 -0500

    ✨ new/shared-configurations: Render component in Storybook.

---
+++ packages/shared-configurations/docs/shared-configurations.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Configurations } from "../lib/shared-configurations";
+
+export default {
+	title: "Configurations"
+}
+
+export const primary = () => <Configurations>Hello World!</Configurations>
