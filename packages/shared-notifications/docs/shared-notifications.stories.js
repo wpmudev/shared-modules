5674cd18fc54783d4a5c7028eea7a290e0247761
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-02-18 20:54:44 -0500

    ✨ new/shared-notifications: Render component in Storybook.

---
+++ packages/shared-notifications/docs/shared-notifications.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Notifications } from "../lib/shared-notifications";
+
+export default {
+	title: "Notifications"
+}
+
+export const primary = () => <Notifications>Hello World!</Notifications>
