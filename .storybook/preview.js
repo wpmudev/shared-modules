88002cb3a75e6eda6064e14a6ae568337987551b
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-02-19 09:53:26 -0500

    🔧 Change Storybook preview default background.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "@wpmudev/shared-ui/dist/css/shared-ui.min.css"; // Get SUI styles.
 
+// Edit parameters.
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	backgrounds: {
+		default: "WordPress",
+		values: [
+			{
+				name: "WordPress",
+				value: "#f1f1f1"
+			}
+		]
+	},
 };
 
+// Edit "preview" markup.
 const Wrapper = ({ children }) => {
 	return (
 		<div className="sui-2-10-4">
