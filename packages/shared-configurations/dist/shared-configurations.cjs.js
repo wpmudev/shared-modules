82bc030f90a2464ff59bc85cccfa54e279189423
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-02-18 20:24:08 -0500

    📦 build/shared-configurations: Compile package.

---
+++ packages/shared-configurations/dist/shared-configurations.cjs.js
@@ -0,0 +1,21 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+var Configurations = function Configurations(_ref) {
+  var children = _ref.children;
+  return /*#__PURE__*/React__default['default'].createElement("div", {
+    style: {
+      padding: 10,
+      backgroundColor: "lightblue"
+    }
+  }, "Say hello: ", children);
+};
+
+exports.Configurations = Configurations;
---
+++ packages/shared-configurations/dist/shared-configurations.esm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Configurations = function Configurations(_ref) {
+  var children = _ref.children;
+  return /*#__PURE__*/React.createElement("div", {
+    style: {
+      padding: 10,
+      backgroundColor: "lightblue"
+    }
+  }, "Say hello: ", children);
+};
+
+export { Configurations };
