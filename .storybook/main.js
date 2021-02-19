72867f127c29996900d04921b155d5fa6482d7c6
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-02-18 21:36:37 -0500

    🐛 Fix Storybook setup by changing order of resolution of main fields.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,21 @@
 module.exports = {
-  "stories": [
-    "../packages/**/*.stories.js"
-  ],
-  "addons": [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-	"@storybook/addon-a11y"
-  ]
-}
+	stories: [
+		"../packages/**/*.stories.js"
+	],
+	addons: [
+		"@storybook/addon-actions",
+		"@storybook/addon-links",
+		"@storybook/addon-a11y"
+	],
+	webpackFinal: async ( config ) => {
+		// Change the order of resolution of main fields.
+		config.resolve.mainFields = [
+			'src',
+			'module',
+			'main'
+		];
+
+		// Return the altered config
+		return config;
+	}
+};
