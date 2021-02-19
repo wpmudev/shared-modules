1c146b2c6497da141c07b8a135ab55cc79974365
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-02-18 20:40:20 -0500

    🔧 Configure Storybook to work from inside packages folder.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,9 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../packages/**/*.stories.js"
   ],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-actions",
+    "@storybook/addon-links"
   ]
 }
