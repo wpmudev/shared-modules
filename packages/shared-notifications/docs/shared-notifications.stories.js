5e3c11680036ae8738bfa36cda8f5c7b9c3142ad
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-02-19 19:30:32 -0500

    ✨ new/shared-notifications: Add `noticeOffer` component to stories.

--- packages/shared-notifications/docs/shared-notifications.stories.js
+++ packages/shared-notifications/docs/shared-notifications.stories.js
@@ -1,8 +1,37 @@
 import React from "react";
-import { Notifications } from "../lib/shared-notifications";
+import { NoticeOffer } from "../lib/shared-notifications";
 
 export default {
-	title: "Notifications"
+	title: "Notifications/Notice Offer",
+	component: NoticeOffer
 }
 
-export const primary = () => <Notifications>Hello World!</Notifications>
+export const primary = ( args ) => {
+	return (
+		<NoticeOffer { ...args }>
+			<p>We’d hate for free users to miss out on Subscription / Recurring Payment Support, so we’re temporarily slashing the price of Forminator Pro.</p>
+		</NoticeOffer>
+	);
+};
+primary.storyName = "Default";
+primary.args = {
+	title: "Don’t Miss Out On Subscription / Recurring Payment Support",
+	price: 65,
+	discount: 60,
+	imagePath: "https://singlesite.test/wp-content/plugins/forminator/assets/images/",
+	imageName: "forminator-create-modal",
+	imageExt: "png",
+	retinaImage: true,
+	disclaimer: "Only admin users can see this message",
+	buttonLabel: "Get 60% Off Forminator Pro",
+	buttonLink: "https://premium.wpmudev.com/"
+};
+primary.argTypes = {
+	discount: {
+		control: {
+			type: "range",
+			min: 0,
+			max: 100
+		}
+	}
+};
