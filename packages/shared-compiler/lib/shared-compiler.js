50d2decca0ebf174c0c6a5bf43c957d9f5588cf3
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-03-15 07:53:56 -0500

    🐛 fix/compiler: Add `rollup-plugin-postcss` dependency.

--- packages/shared-compiler/lib/shared-compiler.js
+++ packages/shared-compiler/lib/shared-compiler.js
@@ -3,6 +3,7 @@ const rollup = require( 'rollup' );
 const path = require( 'path' );
 const resolve = require( '@rollup/plugin-node-resolve' ).default;
 const babel = require( '@rollup/plugin-babel' ).default;
+const postcss = require('rollup-plugin-postcss');
 
 const currentWorkingPath = process.cwd();
 const { src, name } = require( path.join( currentWorkingPath, 'package.json' ) );
