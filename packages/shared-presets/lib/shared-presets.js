c4eb0959fc172db9f6091466266baf35ce1b48d0
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-03-31 00:07:41 -0500

    ✨ new(shared-presets): Call presets page component.

--- packages/shared-presets/lib/shared-presets.js
+++ packages/shared-presets/lib/shared-presets.js
@@ -1,10 +1,11 @@
 import { PresetsTable } from './elements/table';
 import { PresetsAccordion } from './elements/accordion';
 import { PresetsWidget } from './sections/shared-presets-widget';
+import { PresetsPage } from './sections/shared-presets-page';
 
 const Presets = {};
 
 Presets.Table = PresetsTable;
 Presets.Accordion = PresetsAccordion;
 
-export { Presets, PresetsWidget };
+export { Presets, PresetsWidget, PresetsPage };
