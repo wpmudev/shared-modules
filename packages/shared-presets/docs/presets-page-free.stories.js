0d2808e21940c54cf674b87f40e5e2f7f06ea2c5
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-04-09 22:28:30 -0500

    📝 docs(shared-presets): Update argTypes for free stories.

--- packages/shared-presets/docs/presets-page-free.stories.js
+++ packages/shared-presets/docs/presets-page-free.stories.js
@@ -25,13 +25,6 @@ const Template = ({ presets, settings, ...args }) => {
 export const primary = Template.bind({});
 primary.storyName = "Default"
 primary.args = {
-    title: 'Preset Configs',
-    message: 'Configs bundle your Smush settings and make them available to download and apply on your other sites.',
-    welcome: {
-        image: image2x,
-        message: 'Use configs to save preset configurations of Smush’s settings, then upload and apply them to your other sites in just a few clicks! You can easily apply configs to multiple sites at once via the Hub.'
-    },
-    updateMessage: 'Created or updated the configs via the Hub? Re-check to get the updated list.',
     presets: [
         {
             default: true,
@@ -45,11 +38,58 @@ primary.args = {
             image: 'https://pbs.twimg.com/profile_images/671394755951984640/GSkxXgDp_400x400.png'
         },
     ],
-    free: true,
-    freeMessage: 'Tired of saving, downloading and uploading your configs across your sites? WPMU DEV members use The Hub to easily apply configs to multiple sites at once... Try it free today!',
-    freeButton: 'Try The Hub'
 };
-primary.argTypes = {};
+primary.argTypes = {
+    title: {
+        type: {
+            name: 'string',
+            required: true
+        },
+        description: 'Presets Configurations box requires a title.',
+        table: {
+            defaultValue: {
+                summary: 'Presets Configs'
+            }
+        },
+        control: {
+            type: 'text'
+        }
+    },
+    description: {
+        type: {
+            name: 'string',
+            required: false
+        },
+        description: 'A description might be necessary to let users know what the component is about.',
+        defaultValue: 'Configs bundle your Smush settings and make them available to download and apply on your other sites.',
+        control: {
+            type: 'text'
+        }
+    },
+    welcome: {
+        type: {
+            name: 'object',
+            required: false
+        },
+        description: 'Welcome message is a blue notification with dismiss property. To show this element, `message` variable is required. Use `image` variable to show an image next to message.',
+        defaultValue: {
+            image: image2x,
+            message: 'Use configs to save preset configurations of Smush’s settings, then upload and apply them to your other sites in just a few clicks! You can easily apply configs to multiple sites at once via the Hub.'
+        }
+    },
+    free: {
+        type: {
+            name: 'object',
+            required: true
+        },
+        description: '. . .',
+        defaultValue: {
+            state: true,
+            message: 'Tired of saving, downloading and uploading your configs across your sites? WPMU DEV members use The Hub to easily apply configs to multiple sites at once... Try it free today!',
+            button: 'Try The Hub'
+        }
+    },
+};
 
 export const secondary = Template.bind({});
 secondary.storyName = "Empty"
