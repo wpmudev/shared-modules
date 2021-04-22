586e68371887bb16e8fe503c6778d6dd8139f89c
Danae Millan <danae.millan@incsub.com>
2021-04-22 16:28:29 -0300

    Make presets widget a functional component

--- packages/shared-presets/lib/containers/shared-presets-widget.js
+++ packages/shared-presets/lib/containers/shared-presets-widget.js
@@ -1,95 +1,90 @@
-import React, { Component, Children } from 'react';
+import React, { Children } from 'react';
 import { Box, BoxHeader, BoxBody, BoxFooter } from '@wpmudev/react-box';
 import { Notifications } from '@wpmudev/react-notifications';
 import { Button } from '@wpmudev/react-button';
 import { PresetsAccordionItem } from '../components/accordion-item';
 
-export class PresetsWidget extends Component {
-    render() {
-        const { isLoading, children: configsList } = this.props,
-			isEmpty = ! configsList || 0 === configsList.length;
+export const PresetsWidget = ( { isLoading, children: configsList, ...props } ) => {
+	const isEmpty = ! configsList || 0 === configsList.length;
 
-        const items = Children.map( configsList, item => {
-            return (
-                <PresetsAccordionItem
-                    default={ item.props.default || false }
-                    title={ item.props.title }
-                    description={ item.props.description }
-                    image={ item.props.image }
-                    applyLabel={ item.props.applyLabel }
-                    applyAction={ item.props.applyAction }
-                    downloadLabel={ item.props.downloadLabel }
-                    editLabel={ item.props.editLabel }
-                    editAction={ item.props.editAction }
-                    deleteLabel={ item.props.deleteLabel }
-                >
-                    { item.props.children }
-                </PresetsAccordionItem>
-            );
-        });
+	const items = Children.map( configsList, item => (
+		<PresetsAccordionItem
+			default={ item.props.default || false }
+			title={ item.props.title }
+			description={ item.props.description }
+			image={ item.props.image }
+			applyLabel={ item.props.applyLabel }
+			applyAction={ item.props.applyAction }
+			downloadLabel={ item.props.downloadLabel }
+			editLabel={ item.props.editLabel }
+			editAction={ item.props.editAction }
+			deleteLabel={ item.props.deleteLabel }
+		>
+			{ item.props.children }
+		</PresetsAccordionItem>
+	) );
 
-        return (
-            <Box>
+	return (
+		<Box>
 
-                { ! isLoading && ! isEmpty && (
-                    <BoxHeader
-                        titleIcon="wrench-tool"
-                        title={ this.props.title }
-                        tagLabel={ this.props.children.length }
-                    />
-                )}
+			{ ! isLoading && ! isEmpty && (
+				<BoxHeader
+					titleIcon="wrench-tool"
+					title={ props.title }
+					tagLabel={ configsList.length }
+				/>
+			)}
 
-                { isEmpty && (
-                    <BoxHeader
-                        titleIcon="wrench-tool"
-                        title={ this.props.title }
-                    />
-                )}
+			{ isEmpty && (
+				<BoxHeader
+					titleIcon="wrench-tool"
+					title={ props.title }
+				/>
+			)}
 
-                <BoxBody>
+			<BoxBody>
 
-                    <p>{ this.props.message }</p>
+				<p>{ props.message }</p>
 
-                    { ! isLoading && isEmpty && (
-                        <Notifications type="info">
-                            <p>{ this.props.notice }</p>
-                        </Notifications>
-                    )}
+				{ ! isLoading && isEmpty && (
+					<Notifications type="info">
+						<p>{ props.notice }</p>
+					</Notifications>
+				)}
 
-                </BoxBody>
+			</BoxBody>
 
-				{ isLoading && (
-					<div>
-						<span>
-							<span className="sui-icon-loader" aria-hidden="true"></span>
-							{ this.props.loadingLabel }
-						</span>
-					</div>
-				) }
+			{ isLoading && (
+				<div>
+					<span>
+						<span className="sui-icon-loader" aria-hidden="true"></span>
+						{ props.loadingLabel }
+					</span>
+				</div>
+			) }
 
-                { ! isEmpty && (
-                    <div
-                        className="sui-accordion sui-accordion-flushed"
-                        style={ { borderBottom: 0 } }
-                    >
-                        { items }
-                    </div>
-                )}
+			{ ! isEmpty && (
+				<div
+					className="sui-accordion sui-accordion-flushed"
+					style={ { borderBottom: 0 } }
+				>
+					{ items }
+				</div>
+			)}
 
-                <BoxFooter>
-                    <Button
-                        icon="save"
-                        label="Save Config"
-                        color="blue"
-                    />
-                    <Button
-                        icon="wrench-tool"
-                        label="Manage Configs"
-                        design="ghost"
-                    />
-                </BoxFooter>
+			<BoxFooter>
+				<Button
+					icon="save"
+					label="Save Config"
+					color="blue"
+				/>
+				<Button
+					icon="wrench-tool"
+					label="Manage Configs"
+					design="ghost"
+				/>
+			</BoxFooter>
 
-            </Box>
-        );
-    }
+		</Box>
+	);
 }
