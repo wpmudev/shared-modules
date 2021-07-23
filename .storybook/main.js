1a9d38d6972893ffd8bdb86245e75dea40863d8e
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-07-22 20:31:05 -0500

    ⚗️ new(core): Mobile UX Hints.

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
 		"@storybook/addon-links",
 		"@storybook/addon-actions",
 		"@storybook/addon-essentials",
-		"@storybook/addon-a11y"
+		"@storybook/addon-a11y",
+		"storybook-mobile"
 	],
 	// webpackFinal: async ( config ) => {
 	// 	// Change the order of resolution of main fields.
