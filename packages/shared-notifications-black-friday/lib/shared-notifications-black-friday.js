83dda4a283f0fd10ddb72510c5e6f5ac0a9e8e7a
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-10-25 22:30:14 -0500

    🐛 fix(notice-black): Create and style component.

--- packages/shared-notifications-black-friday/lib/shared-notifications-black-friday.js
+++ packages/shared-notifications-black-friday/lib/shared-notifications-black-friday.js
@@ -1,7 +1,49 @@
-'use strict';
+import React, { useState } from "react";
+import { ButtonIcon } from "@wpmudev/react-button-icon";
+import {
+	Container,
+	Ribbon,
+	Body,
+	Content,
+	Button
+} from "./utils";
 
-module.exports = sharedNotificationsBlackFriday;
+export const NoticeBlack = ({
+	sourceLang,
+	children,
+	...props
+}) => {
+	const lang = Object.assign({
+		close: 'Close',
+		off: 'Off',
+		cta: 'See the deal'
+	}, sourceLang );
 
-function sharedNotificationsBlackFriday() {
-    // TODO
+	return (
+		<Container
+			className="sui-notice-black-friday"
+			{ ...props }
+		>
+
+			<ButtonIcon
+				color="white"
+				icon="close"
+				iconSize="md"
+				label={ lang.close }
+			/>
+
+			<Ribbon>50% { lang.off }</Ribbon>
+
+			<Body>
+
+				<Content>
+					{ children }
+				</Content>
+
+				<Button>{ lang.cta }</Button>
+
+			</Body>
+
+		</Container>
+	);
 }
