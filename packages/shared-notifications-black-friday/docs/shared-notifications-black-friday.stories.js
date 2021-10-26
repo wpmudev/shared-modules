5b9bffd1d7223dcc723730761325db10e6ca1ee7
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-10-25 22:37:31 -0500

    📝 docs(notice-black): Load demo component.

---
+++ packages/shared-notifications-black-friday/docs/shared-notifications-black-friday.stories.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { NoticeBlack } from "../lib/shared-notifications-black-friday";
+
+export default {
+	title: "Notifications/Black Friday",
+	component: NoticeBlack
+}
+
+export const Primary = ( args ) => {
+	return (
+		<NoticeBlack { ...args }>
+			<p><strong>Black Friday Offer!</strong> Get Smush Pro for free and much more with 50% OFF WPMU DEV Agency Plan FOREVER</p>
+			<p><small>*Only admin users can see this message</small></p>
+		</NoticeBlack>
+	);
+};
+Primary.storyName = "Default";
+Primary.args = {}
+
+export const Secondary = ( args ) => {
+	return (
+		<NoticeBlack { ...args }>
+			<p><strong>Oferta de Viernes Negro!</strong> Obtén Smush Pro gratis y mucho más con el 50% DE DESCUENTO en el "Plan de Agencia WPMU DEV" POR SIEMPRE</p>
+			<p><small>*Solo los administradores pueden ver este mensaje</small></p>
+		</NoticeBlack>
+	);
+}
+Secondary.storyName = "Translated";
+Secondary.args = {
+	sourceLang: {
+		close: 'Cerrar',
+		off: 'Dscto',
+		cta: 'Ir a la oferta'
+	}
+}
