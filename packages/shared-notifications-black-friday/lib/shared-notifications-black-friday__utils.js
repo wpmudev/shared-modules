526058761bcd147b8e36f50177fc42fa5abac26d
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2021-10-28 02:57:20 -0500

    🐛 fix(notice-black): Create global styles with `styled-components`.

--- packages/shared-notifications-black-friday/lib/shared-notifications-black-friday__utils.js
+++ packages/shared-notifications-black-friday/lib/shared-notifications-black-friday__utils.js
@@ -1,5 +1,4 @@
-import React from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import wave1x from './img/wave.png';
 import wave2x from './img/wave@2x.png';
 
@@ -28,6 +27,173 @@ const maxDevice = {
 export const BlackFriday = {};
 
 // UTILS: Create elements.
+BlackFriday.Global = createGlobalStyle`
+[class*="sui-2-"] .sui-wrap {
+
+	.sui-module-notice-black-friday {
+
+		&__container {
+			overflow: hidden;
+			position: relative;
+			margin: 0 0 20px;
+			padding: 30px 40px;
+			border-radius: 4px;
+			background-color: #07212C;
+			background-image: image-set(
+				url(${wave1x}) 1x,
+				url(${wave2x}) 2x
+			);
+			background-image:
+				image-set(
+					url(${wave1x}) 1x,
+					url(${wave2x}) 2x
+				),
+				linear-gradient(270deg, #222222 0.67%, #07212C 100%);
+
+			@media ${maxDevice.tablet} {
+				text-align: center;
+			}
+
+			@media ${minDevice.tablet} {
+				display: flex;
+				margin: 0 0 30px;
+				flex-flow: row nowrap;
+				align-items: flex-start;
+				padding-left: 0;
+			}
+
+			@media ${minDevice.laptop} {
+				align-items: center;
+			}
+
+			.sui-button-icon {
+				position: absolute;
+				top: 10px;
+				right: 10px;
+				margin-right: 0 !important;
+			}
+		}
+
+		&__ribbon {
+			display: inline-block;
+			padding: 10px 20px 11px;
+			background-color: #6D53AB;
+			color: #4DFD89;
+			font-size: 22px;
+			line-height: 29px;
+			font-weight: bold;
+			letter-spacing: normal;
+
+			@media ${minDevice.tablet} {
+				flex: 0 0 auto;
+				padding: 15px 20px 16px;
+			}
+		}
+
+		&__body {
+			margin: 0 0 30px;
+
+			@media ${minDevice.tablet} {
+				min-width: 1px;
+				flex: 1;
+				margin: 0 15px;
+				padding-right: 65px;
+			}
+
+			@media ${minDevice.laptop} {
+				display: flex;
+				flex-flow: row nowrap;
+				align-items: center;
+				padding-right: 0;
+			}
+		}
+
+		&__content {
+			margin: 30px 0;
+
+			@media ${minDevice.tablet} {
+				margin-top: 0;
+			}
+
+			@media ${minDevice.laptop} {
+				min-width: 1px;
+				flex: 1;
+				margin-bottom: 0;
+			}
+
+			h1, h2, h3, h4, h5, h6, p, small {
+				color: #fff;
+			}
+
+			p {
+				margin: 0 0 15px;
+				font-size: 18px;
+				line-height: 25px;
+
+				@media ${minDevice.tablet} {
+					margin: 0 0 5px;
+				}
+
+				&:last-child {
+					margin: 0;
+				}
+
+				strong {
+					font-weight: 700;
+
+					@media ${maxDevice.tablet} {
+						display: block;
+						margin-bottom: 20px;
+						font-size: 28px;
+						line-height: 29px;
+					}
+				}
+
+				small {
+					margin: 0;
+					display: initial;
+					font-size: 13px;
+					line-height: 22px;
+					font-weight: 300;
+				}
+			}
+		}
+
+		&__link {
+			cursor: pointer;
+			display: inline-block;
+			margin: 0;
+			padding: 5px 30px;
+			border: 0;
+			border-radius: 30px;
+			background: #4DFD89;
+			box-shadow: none;
+			color: #222;
+			font-size: 13px;
+			line-height: 30px;
+			font-weight: bold;
+			text-decoration: none;
+			transition: 0.2s ease;
+
+			&:hover,
+			&:focus {
+				background: #fff;
+				color: #222222 !important;
+			}
+
+			&:focus {
+				box-shadow: 0 0 10px 0 rgba(255,255,255,0.3);
+			}
+
+			@media ${minDevice.laptop} {
+				display: block;
+				flex: 0 0 auto;
+			}
+		}
+	}
+}
+`;
+
 BlackFriday.Container = styled.div`
 [class*="sui-2-"] .sui-wrap & {
 	overflow: hidden;
