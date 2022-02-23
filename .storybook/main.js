4fd7539e3a9ece4f55635042732ba66c785a6a87
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2022-02-23 10:54:25 -0500

    📌 Storybook 6.4.19

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,5 @@
 module.exports = {
-	stories: [
-		"../packages/**/*.stories.js"
-	],
+	stories: ["../packages/**/*.stories.js"],
 	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-actions",
@@ -11,17 +9,7 @@ module.exports = {
 		"storybook-mobile",
 		"@geometricpanda/storybook-addon-badges"
 	],
-	// webpackFinal: async ( config ) => {
-	// 	// Change the order of resolution of main fields.
-	// 	config.resolve.mainFields = [
-	// 		'src',
-	// 		'module',
-	// 		'main'
-	// 	];
-	// 	// Return the altered config
-	// 	return config;
-	// },
-	// core: {
-	// 	builder: "webpack5"
-	// }
+	core: {
+		builder: "webpack5"
+	}
 };
