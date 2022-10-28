656e2dc77f4d7c288aeda9895d0d501f4a6bb799
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-10-28 12:47:05 -0500

    📝 docs(black-friday): Update demos based on latest changes.

--- packages/shared-notifications-black-friday/docs/shared-notifications-black-friday.stories.js
+++ packages/shared-notifications-black-friday/docs/shared-notifications-black-friday.stories.js
@@ -6,33 +6,50 @@ export default {
 	component: NoticeBlack
 }
 
-export const Untranslated = ( args ) => {
+export const Untranslated = ({ ...args }) => {
 	return (
 		<NoticeBlack { ...args }>
-			<p><strong>Black Friday Offer!</strong> Get 11 Pro plugins on unlimited sites and much more with 50% OFF WPMU DEV Agency plan FOREVER</p>
-			<p><small>*Only admin users can see this message</small></p>
+			<p>From the creators of Smush, WPMU DEV's all-in-one platform gives you all the tools and support you need to run and grow a web development business. Trusted by over 50,000 web developers. Limited deals available.</p>
+			<p><small>* Only admin users can see this message</small></p>
 		</NoticeBlack>
 	);
 };
 Untranslated.storyName = "Default";
 Untranslated.args = {
-	link: 'https://wpmudev.com/'
+	content: {
+		off: 'Off',
+		intro: 'Black Friday SPECIAL',
+		title: 'The ultimate WordPress toolkit now at',
+		close: 'Close',
+	},
+	price: 82,
+	discount: 50,
+	action: {
+		label: 'Get now',
+		link: 'https://wpmudev.com/',
+	},
 }
 
 export const Translated = ( args ) => {
 	return (
 		<NoticeBlack { ...args }>
-			<p><strong>¡Oferta de Viernes Negro!</strong> Obtén 11 plugins Pro para sitios ilimitados y mucho más por el 50% DE DESCUENTO en el plan WPMU DEV Agency DE POR VIDA</p>
-			<p><small>*Solo los administradores pueden ver este mensaje</small></p>
+			<p>De los creadores de Smush, la plataforma todo en uno de WPMU DEV te brinda todas las herramientas y el soporte que necesitas para ejecutar y hacer crecer tu negocio de desarrollo web. Con la confianza de más de 50.000 desarrolladores web. Ofertas limitadas disponibles.</p>
+			<p><small>* Solo los administradores pueden ver este mensaje</small></p>
 		</NoticeBlack>
 	);
 }
 Translated.storyName = "Translated";
 Translated.args = {
-	...Untranslated.args,
-	sourceLang: {
-		discount: '50% Dscto',
-		closeLabel: 'Cerrar',
-		linkLabel: 'Ir a la oferta'
-	}
+	content: {
+		off: 'Dscto',
+		intro: 'ESPECIAL de Viernes Negro',
+		title: 'El mejor kit de herramientas de WordPress ahora en',
+		close: 'Cerrar',
+	},
+	price: 82,
+	discount: 50,
+	action: {
+		label: 'Consíguelo ahora',
+		link: 'https://wpmudev.com/',
+	},
 }
