5cfd25743c3c77d70f9688b6add8b7bdffd1c348
Leighton <2328848+iamleigh@users.noreply.github.com>
2022-11-14 13:49:45 -0500

    📝 docs(black-friday): Update demo content.

--- packages/shared-notifications-black-friday/docs/shared-notifications-black-friday.stories.js
+++ packages/shared-notifications-black-friday/docs/shared-notifications-black-friday.stories.js
@@ -6,50 +6,50 @@ export default {
 	component: NoticeBlack
 }
 
-export const Untranslated = ({ ...args }) => {
+export const Primary = ({ ...args }) => {
 	return (
 		<NoticeBlack { ...args }>
-			<p>From the creators of Smush, WPMU DEV's all-in-one platform gives you all the tools and support you need to run and grow a web development business. Trusted by over 50,000 web developers. Limited deals available.</p>
-			<p><small>* Only admin users can see this message</small></p>
+			<p>From the creators of Smush, WPMU DEV's all-in-one platform gives you all the Pro tools and support you need to run and grow a web development business. Trusted by over 50,000 web developers. Limited deals available.</p>
+			<p><small>Don't want to claim the offer right now? <button className="suim-black__link" onClick={() => { console.log( 'Open Black Friday modal.' ) }}>Sign up for exclusive VIP access to future sales</button>.</small></p>
 		</NoticeBlack>
 	);
 };
-Untranslated.storyName = "Default";
-Untranslated.args = {
+Primary.storyName = "Default";
+Primary.args = {
 	content: {
 		off: 'Off',
-		intro: 'Black Friday SPECIAL',
-		title: 'The ultimate WordPress toolkit now at',
+		intro: 'Black Friday offer for WP Businesses and Agencies',
+		title: 'Everything you need to run your WP business for',
 		close: 'Close',
 	},
-	price: 82,
-	discount: 50,
+	price: 3000,
+	discount: 83.5,
 	action: {
-		label: 'Get now',
+		label: 'Get deal',
 		link: 'https://wpmudev.com/',
 	},
 }
 
-export const Translated = ( args ) => {
+export const Secondary = ( args ) => {
 	return (
 		<NoticeBlack { ...args }>
-			<p>De los creadores de Smush, la plataforma todo en uno de WPMU DEV te brinda todas las herramientas y el soporte que necesitas para ejecutar y hacer crecer tu negocio de desarrollo web. Con la confianza de más de 50.000 desarrolladores web. Ofertas limitadas disponibles.</p>
-			<p><small>* Solo los administradores pueden ver este mensaje</small></p>
+			<p>De los creadores de Smush, la plataforma todo en uno de WPMU DEV te brinda todas las herramientas y el soporte que necesitas para ejecutar y hacer crecer tu negocio de desarrollo web. Con la confianza de más de 50,000 desarrolladores web. Ofertas limitadas disponibles.</p>
+			<p><small>¿No quieres reclamar la oferta este rato? <button className="suim-black__link" onClick={() => { console.log( 'Open Black Friday modal.' ) }}>Suscríbete para obtener acceso VIP exclusivo a futuras ventas</button>.</small></p>
 		</NoticeBlack>
 	);
 }
-Translated.storyName = "Translated";
-Translated.args = {
+Secondary.storyName = "Translated";
+Secondary.args = {
 	content: {
 		off: 'Dscto',
-		intro: 'ESPECIAL de Viernes Negro',
-		title: 'El mejor kit de herramientas de WordPress ahora en',
+		intro: 'Oferta de Viernes Negro para Empresas y Agencias de WP',
+		title: 'Todo lo que necesitas para dirigir tu negocio de WP por',
 		close: 'Cerrar',
 	},
-	price: 82,
-	discount: 50,
+	price: 3000,
+	discount: 83.5,
 	action: {
-		label: 'Consíguelo ahora',
+		label: 'Obtener Oferta',
 		link: 'https://wpmudev.com/',
 	},
 }
