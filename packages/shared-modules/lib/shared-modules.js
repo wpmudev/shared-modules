feeaab4eab9d32a2f6a93d72e163346ffbdd8c3d
Leighton <2328848+iamleigh@users.noreply.github.com>
2023-01-19 07:33:37 -0500

    feat(core): Add header and perks modules

--- packages/shared-modules/lib/shared-modules.js
+++ packages/shared-modules/lib/shared-modules.js
@@ -3,11 +3,14 @@ import { NoticeDiscount } from '@wpmudev/shared-notifications-discount';
 import { Presets } from '@wpmudev/shared-presets';
 import { TutorialsList } from '@wpmudev/shared-tutorials-list';
 import { TutorialsSlider } from '@wpmudev/shared-tutorials-slider';
+import { Header, Perks } from '@wpmudev/shared-header';
 
 export {
 	NoticeBlack,
 	NoticeDiscount,
 	Presets,
 	TutorialsList,
-	TutorialsSlider
+	TutorialsSlider,
+	Header,
+	Perks
 };
